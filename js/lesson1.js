fed63ec9f82e203db6bb3a903d0723e74e812f8a
Ihor Ivanets <ivanets22@gmail.com>
2024-06-15 11:33:09 +0300

    completed task-3

--- js/lesson1.js
+++ js/lesson1.js
@@ -19,9 +19,39 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-const num = Number(prompt("Enter number"));
-let hours = parseInt(num / 60);
-let minutes = num % 60;
-hours = String(hours).padStart(2, "0");
-minutes = String(minutes).padStart(2, "0");
-console.log(`${hours}:${minutes}`);
+// const num = Number(prompt("Enter number"));
+// let hours = parseInt(num / 60);
+// let minutes = num % 60;
+// hours = String(hours).padStart(2, "0");
+// minutes = String(minutes).padStart(2, "0");
+// console.log(`${hours}:${minutes}`);
+
+//Напишіть цикл, який виводить в консоль
+//числа от max до min по зменьшенню
+//Виведіть в консоль суму усіх парних чисел
+
+// const min = Number(prompt("Enter min"));
+// const max = Number(prompt("Enter max"));
+// let sum = 0;
+
+// for (let i = max; i >= min; i--) {
+//   console.log(i);
+//   if (i % 2 === 0) {
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+function sumNumber(min, max) {
+  let sum = 0;
+  for (let i = max; i >= min; i--) {
+    console.log(i);
+    if (i % 2 === 0) {
+      sum += i;
+    }
+    return sum;
+  }
+}
+
+console.log(sumNumber(2, 50));
