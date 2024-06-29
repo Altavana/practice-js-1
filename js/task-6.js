9dcec96a10953b203886a182b011ffd424b7bca3
stanislav_nedelchev <stasnedel@gmail.com>
2024-06-29 13:14:44 +0300

    task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,2 +1,14 @@
 // Напишіть функцію, яка повертає новий масив без вказаних значень.Використовуйте примітиви
 // Очікується [1, 2, 3, 1, 2] без [1, 2] => [3]
+
+function createNewArr(firstArr, secondArr) {
+  const newArr = [];
+  for (const elem of firstArr) {
+    if (!secondArr.includes(elem)) {
+      newArr.push(elem);
+    }
+  }
+  return newArr;
+}
+
+console.log(createNewArr([1, 2, 3, 1, 2], [1, 2]));
