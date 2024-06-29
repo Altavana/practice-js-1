a2e02f837cbceffdef71ea2c4080624a0a817488
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-06-29 10:45:53 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,19 @@
 // todo Створіть об'єкт calculator із трьома методами
 // read(a, b) - приймає два аргументи і зберігає їх як властивості об'єкта
 // sum() повертає суму збережених значень
-// mult() помножує збережені значення і повертає результат
+// mult() помножує збережені значення і повертає резульco
+const calculator = {
+  read(a, b) {
+    this.first = a;
+    this.second = b;
+  },
+  sum() {
+    return this.first + this.second;
+  },
+  mult() {
+    return this.first * this.second;
+  },
+};
+calculator.read(1, 7);
+console.log(calculator.sum());
+console.log(calculator.mult());
